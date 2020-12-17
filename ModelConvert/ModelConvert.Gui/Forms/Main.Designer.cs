a2291a77b506912258ae487e2de835b1772942a4
Drew Miller <drew.miller35@yahoo.com>
2020-12-16 22:59:12 -0600

    Main form design

--- ModelConvert/ModelConvert.Gui/Forms/Main.Designer.cs
+++ ModelConvert/ModelConvert.Gui/Forms/Main.Designer.cs
@@ -29,12 +29,86 @@ namespace ModelConvert.Gui.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblBottomDivider = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(107, 20);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(485, 31);
+            this.txtInput.TabIndex = 0;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(12, 23);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(89, 25);
+            this.lblInput.TabIndex = 1;
+            this.lblInput.Text = "Input File:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(598, 18);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(112, 34);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            // 
+            // lblBottomDivider
+            // 
+            this.lblBottomDivider.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBottomDivider.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblBottomDivider.Location = new System.Drawing.Point(12, 90);
+            this.lblBottomDivider.Name = "lblBottomDivider";
+            this.lblBottomDivider.Size = new System.Drawing.Size(698, 2);
+            this.lblBottomDivider.TabIndex = 6;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvert.Location = new System.Drawing.Point(598, 102);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(112, 34);
+            this.btnConvert.TabIndex = 7;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(722, 148);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblBottomDivider);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.txtInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
             this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblBottomDivider;
+        private System.Windows.Forms.Button btnConvert;
     }
 }
